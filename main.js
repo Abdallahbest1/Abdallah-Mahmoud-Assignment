06a74f887fcbb52e8edb0608bb8f1083522d7049
Abdallah Mahmoud Abou Hussein <abdallah2006ok@gmail.com>
2026-03-13 21:26:18 +0200

    Fixes function 8 for edge cases

--- main.js
+++ main.js
@@ -225,7 +225,7 @@ function getTotalActiveHoursPerMonth(textFile, driverID, month) {
     const h = Math.floor(totalSeconds / 3600);
     const m = Math.floor((totalSeconds % 3600) / 60);
     const s = totalSeconds % 60;
-    return `${String(h).padStart(2, "0")}:${String(m).padStart(2, "0")}:${String(s).padStart(2, "0")}`;
+return `${h}:${String(m).padStart(2, "0")}:${String(s).padStart(2, "0")}`;
 
 }
 
